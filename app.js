a77fa547353e74ec09c6eb8e0f544f9b6ed911e1
ehzawad <ehzawad@gmail.com>
2018-04-04 18:08:58 +0600

    templating engine added aka. pug added aka. view added

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 // application root router
-app.get('/', (req, res) => res.send('Hi, I am Zawad'));
+// app.get('/', (req, res) => res.send('Hi, I am Zawad'));
+// now renders a templating engine
+app.set('view engine', 'pug');
+app.get('/', (req, res) => res.render('index'));
 
 // application /hello router
 app.get('/hello', (req, res) => res.send('<h1> Hello Express.js</h1>'));
